1deffe3e3c5c8b24349510724f3c119bd028c3ed
Brenda Salto <55170995+BrendaSalto@users.noreply.github.com>
2022-09-26 22:57:43 -0300

    Eventos y manejo de DOM
    Se agregan productos al carrito.

---
+++ stock.js
@@ -0,0 +1,50 @@
+const productos = [
+    {
+        id:1,
+        nombre:"Hellfish-Simpsons",
+        desc:"Infantil, 1 plaza 1/2",
+        precio:4000,
+        img:"infantil.jpg",
+        cantidad:1
+    },
+    {
+        id:2,
+        nombre:"Lilo y stitch",
+        desc:"Infantil, 1 plaza y 1/2",
+        precio:4000,
+        img:"infantil2.jpg",
+        cantidad:1
+    },
+    {
+        id:3,
+        nombre:"Stranger Things",
+        desc:"adolescente, 1 plaza y 1/2",
+        precio:6000,
+        img:"adolescente.jpg",
+        cantidad:1
+    },
+    {
+        id:4,
+        nombre:"Harry Potter",
+        desc:"adolescente, 1 plaza y 1/2",
+        precio:6000,
+        img:"adolescente2.jpg",
+        cantidad:1
+    },
+    {
+        id:5,
+        nombre:"Pastelito horneado de canela",
+        desc:"Adulto 2 plazas y 1/2",
+        precio:9000,
+        img:"adulto.jpg",
+        cantidad:1
+    },
+    {
+        id:6,
+        nombre:"Homero y Marge",
+        desc:"Adulto 2 plazas y 1/2",
+        precio:9000,
+        img:"adulto.jpg",
+        cantidad:1
+    },
+]
